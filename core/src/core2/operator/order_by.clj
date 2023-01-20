72bcbbebe6b3d91779aabec35ac8c470d10c1eec
FiVo <finn.volkel@gmail.com>
2023-01-20 17:51:01 +0100

    wip

--- core/src/core2/operator/order_by.clj
+++ core/src/core2/operator/order_by.clj
@@ -5,9 +5,11 @@
             [core2.logical-plan :as lp]
             [core2.util :as util]
             [core2.vector.indirect :as iv]
-            [core2.vector.writer :as vw])
+            [core2.vector.writer :as vw]
+            [core2.vector.indirect :as indirect])
   (:import core2.ICursor
            core2.vector.IIndirectRelation
+           (core2.vector.indirect DirectVector IndirectVector)
            (java.io InputStream OutputStream ObjectOutputStream ObjectInputStream DataInputStream DataOutputStream
                     ByteArrayInputStream ByteArrayOutputStream)
            (java.util Arrays Comparator)
@@ -38,29 +40,40 @@
 
 (set! *unchecked-math* :warn-on-boxed)
 
-(def ^:private spill-threshold 16)
 
-(defn take-blocks ^core2.vector.IIndirectRelation [n allocator ^ICursor in-cursor]
-  (let [rel-writer (vw/->rel-writer allocator)]
-    (try
-      (loop [i 0]
-        (when (and (< i spill-threshold)
-                   (.tryAdvance in-cursor
-                                (reify Consumer
-                                  (accept [_ src-rel]
-                                    (vw/append-rel rel-writer src-rel)))))
-          (recur (inc i))))
+(defn- sorted-idxs ^ints [^IIndirectRelation read-rel, order-specs]
+  (-> (IntStream/range 0 (.rowCount read-rel))
+      (.boxed)
+      (.sorted (reduce (fn [^Comparator acc, [column {:keys [direction null-ordering]
+                                                      :or {direction :asc, null-ordering :nulls-last}}]]
+                         (let [read-col (.vectorForName read-rel (name column))
+                               col-comparator (expr.comp/->comparator read-col read-col null-ordering)
 
-      (catch Exception e
-        (.close rel-writer)
-        (throw e)))
+                               ^Comparator
+                               comparator (cond-> (reify Comparator
+                                                    (compare [_ left right]
+                                                      (.applyAsInt col-comparator left right)))
+                                            (= :desc direction) (.reversed))]
+                           (if acc
+                             (.thenComparing acc comparator)
+                             comparator)))
+                       nil
+                       order-specs))
+      (.mapToInt (reify ToIntFunction
+                   (applyAsInt [_ x] x)))
+      (.toArray)))
 
-    (vw/rel-writer->reader rel-writer)))
 
 (comment
   (def buffer-allocator (sc.api/letsc [1 -1] allocator))
   (def direct-vec (sc.api/letsc [1 -1] (-> (seq read-rel) first)))
-  (def direct-rel (sc.api/letsc [1 -1] (->  first)))
+  (def direct-vec (sc.api/letsc [1 -1] (-> (seq read-rel) second)))
+
+  (=
+   (-> direct-vec :v (.getField) (.getName))
+   (-> direct-vec :name))
+
+  (def direct-rel (sc.api/letsc [1 -1] read-rel))
 
   (sc.api/letsc [1 -1]
                 read-rel
@@ -81,48 +94,165 @@
         dos (DataOutputStream. os)]
     (.writeInt dos (count (.getBytes name)))
     (.write dos (.getBytes name))
+    (.flush dos)
     (.start writer)
     (.writeBatch writer)
     (.end writer)))
 
 (defn read-dvec [^BufferAllocator allocator ^InputStream is]
-  (let [
-        dis (DataInputStream. is)
+  (let [dis (DataInputStream. is)
         name-length (.readInt dis)
         ba (byte-array name-length)
-        name (.read dis ba 0 name-length)
+        _ (.read dis ba 0 name-length)
+        name (String. ba)
         reader (ArrowStreamReader. is allocator)
         ^VectorSchemaRoot read-root (.getVectorSchemaRoot reader)]
     (.loadNextBatch reader)
     (iv/->DirectVector (.getVector read-root 0) name)))
 
+
+(defn ivec-remove-indirection [^BufferAllocator allocator ^core2.vector.IIndirectVector ivec]
+  (cond->> ivec
+    (instance? IndirectVector ivec) (indirect/indirect-vec->direct-vec allocator)))
+
+(defn irel-remove-indirection [^BufferAllocator allocator ^core2.vector.IIndirectVector irel]
+  (->> (seq irel) (map #(ivec-remove-indirection allocator %)) indirect/->indirect-rel))
+
+(defn write-irel [^core2.vector.IIndirectRelation irel ^OutputStream os column-order]
+  (let [ivecs (for [column-name column-order]
+                (.vectorForName irel column-name))
+        root (VectorSchemaRoot. (map #(.getField %) ivecs) ivecs)
+        writer (ArrowStreamWriter. root nil (Channels/newChannel os))]
+    (.start writer)
+    (.writeBatch writer)
+    (.end writer)))
+
+(defn read-irel [^BufferAllocator allocator ^InputStream is column-order]
+  (let [reader (ArrowStreamReader. is allocator)
+        ^VectorSchemaRoot read-root (.getVectorSchemaRoot reader)]
+    (.loadNextBatch reader)
+    (doall (map-indexed (fn [i column-name] (iv/->DirectVector (.getVector read-root 0) column-name))) column-order)))
+
+(defn sort-irel [^BufferAllocator allocator ^core2.vector.IIndirectRelation irel order-specs]
+  (let [sorted-idxs (sorted-idxs irel order-specs)]
+    (->> (iv/select irel sorted-idxs) (irel-remove-indirection allocator))))
+
+(def ^:private split-threshold 200000)
+
+(defn mk-irel-comparator [^core2.vector.IIndirectRelation irel1 ^core2.vector.IIndirectRelation irel2 order-specs]
+  (reduce (fn [^Comparator acc, [column {:keys [direction null-ordering]
+                                         :or {direction :asc, null-ordering :nulls-last}}]]
+            (let [read-col1 (.vectorForName irel1 (name column))
+                  read-col2 (.vectorForName irel2 (name column))
+                  col-comparator (expr.comp/->comparator read-col1 read-col2 null-ordering)
+
+                  ^Comparator
+                  comparator (cond-> (reify Comparator
+                                       (compare [_ left right]
+                                         (.applyAsInt col-comparator left right)))
+                               (= :desc direction) (.reversed))]
+              (if acc
+                (.thenComparing acc comparator)
+                comparator)))
+          nil
+          order-specs))
+
+(defn two-merge-irels [irel1 irel2 order-specs]
+  (let [out-rel-writer (vw/->rel-writer allocator)
+        compare-fn
+        row-copiers (into [] (map #(.rowCopier out-rel-writer %)) [irel1 irel2])]
+    ()))
+
+(fn [^Comparator acc, [column {:keys [direction null-ordering]
+                               :or {direction :asc, null-ordering :nulls-last}}]]
+  (let [read-col (.vectorForName read-rel (name column))
+        col-comparator (expr.comp/->comparator read-col read-col null-ordering)
+
+        ^Comparator
+        comparator (cond-> (reify Comparator
+                             (compare [_ left right]
+                               (.applyAsInt col-comparator left right)))
+                     (= :desc direction) (.reversed))]
+    (if acc
+      (.thenComparing acc comparator)
+      comparator)))
+
+
+
+(comment
+  (sc.api/letsc [7 -6]
+                ;; (.getSchema read-root)
+                (count (.getFieldVectors read-root))))
+
 (comment
   (def baos (ByteArrayOutputStream.))
-  (def bais (ByteArrayInputStream. (.toByteArray baos)))
   (write-dvec direct-vec baos)
+  (def bais (ByteArrayInputStream. (.toByteArray baos)))
   (read-dvec buffer-allocator bais))
 
-(defn write-irel [^core2.vector.IIndirectRelation irel ^OutputStream os]
+(defn write-drel [^core2.vector.IIndirectRelation irel ^OutputStream os]
   (let [dvecs (seq irel)
         dos (DataOutputStream. os)]
     (.writeInt dos (count dvecs))
-    (doseq [dvec (seq irel)]
+    (.flush dos)
+    (doseq [dvec (take 1 (seq irel))]
       (write-dvec dvec os))))
 
-(defn read-irel [^BufferAllocator allocator ^InputStream is]
+(defn read-drel [^BufferAllocator allocator ^InputStream is]
   (let [dis (DataInputStream. is)
         relation-size (.readInt dis)]
+    (println relation-size)
     (iv/->indirect-rel (repeatedly relation-size #(read-dvec allocator is)))))
+
 
 (comment
   (def baos (ByteArrayOutputStream.))
-  (def bais (ByteArrayInputStream. (.toByteArray baos)))
-  (write-dvec direct-vec baos)
-  (read-dvec buffer-allocator bais))
+  (write-drel direct-rel baos)
+  (def bais (ByteArrayInputStream. (.toByteArray ^ByteArrayOutputStream baos)))
+  (read-drel buffer-allocator bais))
+
+(defn- irel->column-names [irel]
+  (map :name (seq irel)))
+
+(comment
+  (irel->column-names direct-rel))
+
+;; (defn write-drel2 [^core2.vector.IIndirectRelation irel ^OutputStream os]
+;;   (let [vs (map #(.getVector %) (seq irel))
+;;         root (VectorSchemaRoot. [(.getField v)] [v])
+;;         writer (ArrowStreamWriter. root nil (Channels/newChannel os))]
+
+;;     )
+
+;;   )
+
+;; (let [dvecs (seq irel)
+;;       dos (DataOutputStream. os)]
+;;   (.writeInt dos (count dvecs))
+;;   (.flush dos)
+;;   (doseq [dvec (take 1 (seq irel))]
+;;     (write-dvec dvec os)))
+
+;; reader (ArrowStreamReader. is allocator)
+;; ^VectorSchemaRoot read-root (.getVectorSchemaRoot reader)
 
 
-(defn file->mmap-irel [path]
-  )
+(defn take-blocks ^core2.vector.IIndirectRelation [n allocator ^ICursor in-cursor]
+  (let [rel-writer (vw/->rel-writer allocator)]
+    (try
+      (loop [i 0]
+        (when (and (< i spill-threshold)
+                   (.tryAdvance in-cursor
+                                (reify Consumer
+                                  (accept [_ src-rel]
+                                    (vw/append-rel rel-writer src-rel)))))
+          (recur (inc i))))
+
+      (catch Exception e
+        (.close rel-writer)
+        (throw e)))
+
+    (vw/rel-writer->reader rel-writer)))
 
 (defn- accumulate-relations ^core2.vector.IIndirectRelation [allocator ^ICursor in-cursor]
   (let [rel-writer (vw/->rel-writer allocator)]
@@ -170,9 +300,12 @@
 
 (comment
   (def indirect-relation
-    (sc.api/letsc [5 -2]
+    (sc.api/letsc [1 -2]
                   read-rel
                   #_order-specs))
+  (-> indirect-relation seq first :v seq)
+
+  (sc.api/letsc [2 -2] (-> indirect-relation seq first :v (.get 0)))
 
   (write-object indirect-relation (io/file "test.xtdb"))
   (def var-char-vec (-> (seq indirect-relation) first :v))
@@ -187,25 +320,103 @@
 
 (def ^:private row-limit 1000)
 
+
+(comment
+  '(tryAdvance [_ c]
+               (with-open [read-rel (accumulate-relations allocator in-cursor)]
+                 (if (pos? (.rowCount read-rel))
+                   (with-open [out-rel (iv/select read-rel (sorted-idxs read-rel order-specs))]
+                     (.accept c out-rel)
+                     true)
+                   false))))
+
+
+(defn split-blocks ^core2.vector.IIndirectRelation [n allocator ^ICursor in-cursor]
+  (let [rel-writer (vw/->rel-writer allocator)
+        blocks-taken (try
+                       (loop [i 0]
+                         (if (and (< i n)
+                                  (.tryAdvance in-cursor
+                                               (reify Consumer
+                                                 (accept [_ src-rel]
+                                                   (vw/append-rel rel-writer src-rel)))))
+                           (recur (inc i))
+                           i))
+
+                       (catch Exception e
+                         (.close rel-writer)
+                         (throw e)))]
+
+    (vw/rel-writer->reader rel-writer)))
+
+
+
+(defn calculate-out-rels [^BufferAllocator allocator ^ICursor in-cursor]
+
+
+
+  )
+
 (deftype OrderByCursor [^BufferAllocator allocator
                         ^ICursor in-cursor
-                        order-specs]
+                        order-specs
+                        ^:unsynchronized-mutable ^boolean consumed?
+                        ^:unsynchronized-mutable out-rels]
   ICursor
   (tryAdvance [_ c]
     (with-open [read-rel (accumulate-relations allocator in-cursor)]
-      (sc.api/spy)
       (if (pos? (.rowCount read-rel))
         (with-open [out-rel (iv/select read-rel (sorted-idxs read-rel order-specs))]
           (.accept c out-rel)
           true)
         false)))
+  #_(tryAdvance [this c]
+      (cond consumed? false
+
+            out-rels
+            (if-let [out-rel (first out-rels)]
+              (try
+                (set! (.out-rels this) (lazy-seq (next out-rels)))
+                (.accept c out-rel)
+                true
+                (finally
+                  (.close out-rel)))
+              (do
+                (set! (.out-rels this) nil)
+                (set! (.consumed? this) true)
+                false))
+
+            :else
+            (let [out-rels (calculate-out-rels allocator in-cursor)]
+              (if-let [out-rel (first out-rels)]
+                (try
+                  (set! (.out-rels this) (lazy-seq (next out-rels)))
+                  (.accept c out-rel)
+                  true
+                  (finally
+                    (.close out-rel)))
+                (do
+                  (set! (.out-rels this) nil)
+                  (set! (.consumed? this) true)
+                  false)))))
 
   (close [_]
     (util/try-close in-cursor)))
 
+
+(defn test-lazy-seq [n]
+  (if (zero? n)
+    '()
+    (lazy-seq (println n) (cons n (test-lazy-seq (dec n))))))
+
+
+(def t (test-lazy-seq 10))
+
+(def t2 (next t))
+
 (defmethod lp/emit-expr :order-by [{:keys [order-specs relation]} args]
   (lp/unary-expr (lp/emit-expr relation args)
-    (fn [col-types]
-      {:col-types col-types
-       :->cursor (fn [{:keys [allocator]} in-cursor]
-                   (OrderByCursor. allocator in-cursor order-specs))})))
+      (fn [col-types]
+        {:col-types col-types
+         :->cursor (fn [{:keys [allocator]} in-cursor]
+                     (OrderByCursor. allocator in-cursor order-specs false nil))})))
